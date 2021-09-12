16e8643450460fd320f1fc3161043f3c4c99f4e4
kurudrive <kurudrive@gmail.com>
2021-09-12 11:17:46 +0900

    sassファイル設定

--- functions.php
+++ functions.php
@@ -1,1 +1,46 @@
 <?php
+/**
+ * Lightning Child theme functions
+ *
+ * @package lightning
+ */
+
+/************************************************
+ * 独自CSSファイルの読み込み処理
+ *
+ * 主に CSS を SASS で 書きたい人用です。 素の CSS を直接書くなら style.css に記載してかまいません.
+ */
+
+// 独自のCSSファイル（assets/css/）を読み込む場合は true に変更してください.
+$my_lightning_additional_css = true;
+
+if ( $my_lightning_additional_css ) {
+	// 公開画面側のCSSの読み込み.
+	add_action(
+		'wp_enqueue_scripts',
+		function() {
+			wp_enqueue_style(
+				'my-lightning-custom',
+				get_stylesheet_directory_uri() . '/assets/css/style.css',
+				array( 'lightning-design-style' ),
+				filemtime( dirname( __FILE__ ) . '/assets/css/style.css' )
+			);
+		}
+	);
+	// 編集画面側のCSSの読み込み.
+	add_action(
+		'enqueue_block_editor_assets',
+		function() {
+			wp_enqueue_style(
+				'my-lightning-editor-custom',
+				get_stylesheet_directory_uri() . '/assets/css/editor.css',
+				array( 'wp-edit-blocks', 'lightning-gutenberg-editor' ),
+				filemtime( dirname( __FILE__ ) . '/assets/css/editor.css' )
+			);
+		}
+	);
+}
+
+/************************************************
+ * 独自の処理を必要に応じて書き足します
+ */
--- readme.md
+++ readme.md
@@ -3,3 +3,19 @@
 カスタマイズの方法については以下で順次公開しています
 
 https://training.vektor-inc.co.jp/courses/lightning-customize/
+
+## CSSのビルド
+
+※ 普段からsassのコンパイルをしている人向けの説明です
+
+パッケージのインストール
+
+```
+npm install
+```
+
+assets/_scss/ ディレクトリの監視・コンパイル
+
+```
+npm run watch
+```
