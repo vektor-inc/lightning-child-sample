24510636e70f870ee6920b7c6085da93eafe3923
kurudrive <kurudrive@gmail.com>
2021-09-27 21:33:12 +0900

    phpunit 追加

---
+++ tests/test-sample.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Lightning_Customize_Plugin
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$return = 'aaa';
+		$this->assertEquals( 'aaa', $return );
+	}
+}
